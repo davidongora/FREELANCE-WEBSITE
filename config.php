75cb740083e9565c181f4c83429e6747f7c62621
David <davidongora@gmail.com>
2023-06-14 22:07:33 +0300

    Feat:created the configuration file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$db_host = "us-cdbr-east-06.cleardb.net";
+$db_user = "bf09b363be88c1";
+$db_pass = "0fd61bd3";
+$db_name = "heroku_8c0c2436bd99252";
+
+$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
+
+?>
