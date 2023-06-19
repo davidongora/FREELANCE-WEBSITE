1ab064eab7da22f8a4a448a96fc31db6e0f0d10f
David <ongoradavid5@gmail.com>
2023-06-19 09:22:36 +0300

    removed the close connect

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ $db_pass = "0fd61bd3";
 $db_name = "heroku_8c0c2436bd99252";
 
 $connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
-mysqli_close($connect);
+
 
 ?>
